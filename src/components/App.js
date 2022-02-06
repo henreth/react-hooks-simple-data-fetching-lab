08bce931e7cd9ab079330aecb59df17a89bcd1ac
Henry Miller <mail.henrym+git@gmail.com>
2022-02-06 14:40:49 -0500

    commit tree 9dedbcf8396c13c5f9a909c9cdcdbb2d43f06f36

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useState,useEffect  } from "react";
+
+const dogURL = 'https://dog.ceo/api/breeds/image/random';
+
+function App () {
+    const [randomDog, setRandomDog] = useState({});
+    const [isLoaded, setIsLoaded] = useState(false);
+  
+    useEffect(() => {
+      fetch(dogURL)
+        .then((response) => response.json())
+        .then((data) => {
+          setRandomDog(data.message);
+          setIsLoaded(true);
+        });
+    }, []);
+  
+    // if the data hasn't been loaded, show a loading indicator
+    if (!isLoaded) return <p>Loading...</p>;
+  
+    return <img src={randomDog} alt='A Random Dog'></img>
+}
+
+export default App;
